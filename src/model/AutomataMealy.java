0b860dc15eb5deddb682b2df1cd882fc156efa6d
Santiagokmids <sntgtrochez@gmail.com>
2022-09-24 22:14:39 -0500

    Classes are renamed and defined

--- src/model/AutomataMealy.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public class AutomataMealy extends Automata{
-
-}
---
+++ src/model/FiniteStateMachine.java
@@ -0,0 +1,15 @@
+package model;
+
+public class FiniteStateMachine {
+	
+	public FiniteStateMachine() {
+	}
+	
+	public boolean createMealyMachine() {
+		return false;
+	}
+	
+	public boolean createMooreMachine() {
+		return false;
+	}
+}
