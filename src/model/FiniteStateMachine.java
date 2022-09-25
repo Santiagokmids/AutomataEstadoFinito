ba2c8d5723d1a46e1c95a729eca432b99081bf19
Santiagokmids <sntgtrochez@gmail.com>
2022-09-25 14:01:09 -0500

    New method of verify states has been added

--- src/model/FiniteStateMachine.java
+++ src/model/FiniteStateMachine.java
@@ -12,4 +12,27 @@ public class FiniteStateMachine {
 	public boolean createMooreMachine() {
 		return false;
 	}
+	
+	public boolean getAtributes(String states, String inputs) {
+		String[] newStates = states.split(",");
+		String[] newInputs = states.split(",");
+		
+		boolean verify = verifyStates(newStates);
+		
+		return verify;
+	}
+	
+	public boolean verifyStates(String[] states) {
+		
+		boolean verify = false;
+
+		for (int i = 0; i < states.length && !verify; i++) {
+			
+			if(Character.isDigit(states[i].charAt(0))) {
+				verify = true;
+			}
+		}
+
+		return verify;
+	}
 }
--- src/ui/FiniteStateMachineGUI.java
+++ src/ui/FiniteStateMachineGUI.java
@@ -2,9 +2,13 @@ package ui;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import model.FiniteStateMachine;
@@ -15,6 +19,24 @@ public class FiniteStateMachineGUI {
 	
 	private Stage window;
 	
+	@FXML
+    private Button btnMealy;
+
+    @FXML
+    private Button btnMoore;
+
+    @FXML
+    private Button btnOutput;
+
+    @FXML
+    private Label labelMachine;
+
+    @FXML
+    private TextField txtInputs;
+
+    @FXML
+    private TextField txtStates;
+
 	@FXML
     private BorderPane mainPane;
 	
@@ -33,4 +55,27 @@ public class FiniteStateMachineGUI {
 		mainPane.getChildren().clear();
 		mainPane.setTop(load);
 	}
+	
+	@FXML
+    void labelToMealy(ActionEvent event) {
+		labelMachine.setText("Autómata de Mealy");
+    }
+
+    @FXML
+    void labelToMoore(ActionEvent event) {
+    	labelMachine.setText("Autómata de Moore");
+    }
+    
+    @FXML
+    void goToInputs(ActionEvent event) {
+    	boolean checkText = finiteStateMachine.getAtributes(txtStates.getText(), txtInputs.getText());
+    	
+    	if (!checkText) {
+			//cambio de ventana
+		} else {
+			//alerta
+		}
+    }
+
+
 }
